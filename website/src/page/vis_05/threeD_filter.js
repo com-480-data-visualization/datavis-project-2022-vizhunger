e9f16b398a39945097d1ec189db8d4bfd34b98d8
Paulina Tao <paulina@example.com>
2022-05-19 01:06:18 +0200

    Trying to make a 3D scene

--- website/src/page/vis_05/threeD_filter.js
+++ website/src/page/vis_05/threeD_filter.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import vis8 from '../../img/vis8.jpeg';
+import React, {Suspense} from 'react';
+import vis6 from '../../img/vis8.jpeg';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Html, useGLTF, RoundedBox } from '@react-three/drei'
+
+// const Model = () => {
+//     const gltf = useGLTF('../../model/telescope/scene.gltf', true)
+//     return <primitive object={gltf.scene} dispose={null}/>
+// }
+
 
 const ThreeDFilter = () => {
     return (
-        <div>
-            <h1>Try explorting the data space yourself!</h1>
-            <p>
-                Don't know what you are looking for? Try playing around a bit!
-            </p>
-            <img src={vis8} />
-        </div>
+        <Canvas colorManagement camera={{position: [1,1,1], fov: 100}}>
+            <Html>
+                <div>
+                    <h1>Try exploring the data space yourself!</h1>
+                    <p>
+                        Don't know what you are looking for? Try playing around a bit!
+                    </p>
+                </div>
+            </Html>
+            <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4}>
+                <meshPhongMaterial color="#f3f3f3" wireframe />
+            </RoundedBox>
+        </Canvas>
     );
 }
 
